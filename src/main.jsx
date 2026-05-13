7792217d9c46a589224b99f71ad4774dc27ce25c
Indrayaza Z <zainuddinindrayaza@gmail.com>
2026-05-14 01:45:46 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // Jika Anda menggunakan file CSS terpisah
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
